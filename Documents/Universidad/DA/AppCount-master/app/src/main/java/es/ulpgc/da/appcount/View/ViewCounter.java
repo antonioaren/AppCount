9b57158fc67ae8c765129832026564871a494307
antonioaren <antonioaren@hotmail.com>
2017-02-05 19:50:03 +0000

    Puntero nullo en el display.

--- Documents/Universidad/DA/AppCount-master/app/src/main/java/es/ulpgc/da/appcount/View/ViewCounter.java
+++ Documents/Universidad/DA/AppCount-master/app/src/main/java/es/ulpgc/da/appcount/View/ViewCounter.java
@@ -21,24 +21,46 @@ public class ViewCounter extends Activity{
     private PresenterCounter presenter;
 
 
+    class MyButtonListener implements View.OnClickListener{
+
+
+        @Override
+        public void onClick(View view) {
+
+            switch (view.getId()){
+                   case R.id.button1:
+                       presenter.addButtonPressed();
+                       break;
+                   case R.id.button2:
+                       presenter.addButtonPressed();
+                       break;
+               }
+        }
+
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        presenter = new PresenterCounter();
 
         botonMas = (Button) findViewById(R.id.button1);
         botonMenos = (Button) findViewById(R.id.button2);
 
         display = (TextView) findViewById(R.id.display);
 
-        presenter = new PresenterCounter();
-
-
-        botonMas.setOnClickListener(v -> presenter.addButtonPressed());
+        botonMas.setOnClickListener(new MyButtonListener());
+        botonMenos.setOnClickListener(new MyButtonListener());
     }
 
+
+
     public void Display(String text){
-       display.setText(text);
+        display.setText(text);
     }
 
 }
